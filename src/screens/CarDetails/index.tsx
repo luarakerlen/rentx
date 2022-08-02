ab6384887167dbbc618ddb5b7d6a4dd9a9218aa0
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-02 13:31:52 -0300

    create car details screen

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BackButton } from '../../components';
+
+import { Container, Header } from './styles';
+
+export function CarDetails() {
+	return (
+		<Container>
+			<Header>
+				<BackButton onPress={() => {}} />
+			</Header>
+		</Container>
+	);
+}
