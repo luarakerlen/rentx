e02797fddf163d2fb37b395ac7753fd3d16e28e3
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-02 17:00:47 -0300

    update car details screen layout

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import { BackButton } from '../../components';
+import { BackButton, ImageSlider } from '../../components';
 
-import { Container, Header } from './styles';
+import {
+	CarImages,
+	Container,
+	Header,
+	Content,
+	Details,
+	Description,
+	Brand,
+	Name,
+	Rent,
+	Period,
+	Price,
+	About,
+} from './styles';
 
 export function CarDetails() {
 	return (
@@ -9,6 +22,34 @@ export function CarDetails() {
 			<Header>
 				<BackButton onPress={() => {}} />
 			</Header>
+
+			<CarImages>
+				<ImageSlider
+					imagesUrl={[
+						'https://www.pngarts.com/files/3/Audi-Transparent-Background-PNG.png',
+					]}
+				/>
+			</CarImages>
+
+			<Content>
+				<Details>
+					<Description>
+						<Brand>Lamburghini</Brand>
+						<Name>Huracan</Name>
+					</Description>
+
+					<Rent>
+						<Period>Ao dia</Period>
+						<Price>R$ 580</Price>
+					</Rent>
+				</Details>
+
+				<About>
+					Este é automóvel desportivo. Surgiu do lendário touro de lide
+					indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+					para quem gosta de acelerar.
+				</About>
+			</Content>
 		</Container>
 	);
 }
