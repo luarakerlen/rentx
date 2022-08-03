02a66918a7f4f39df7a3fab3fde19022d27c8cb3
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-02 22:52:12 -0300

    create confirm button component

---
+++ src/components/ConfirmButton/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Title } from './styles';
+
+export function ConfirmButton() {
+	return (
+		<Container>
+			<Title>OK</Title>
+		</Container>
+	);
+}
