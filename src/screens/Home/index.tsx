64a6d029e77e6f7ec507114e607efc3150a849ad
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-04 00:30:40 -0300

    add my cars button at home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { Ionicons } from '@expo/vector-icons';
 
 import { api } from '../../services/api';
 import Logo from '../../assets/logo.svg';
 import { Car, Load } from '../../components';
 
-import { CarList, Container, Header, HeaderContent, TotalCars } from './styles';
+import {
+	CarList,
+	Container,
+	Header,
+	HeaderContent,
+	TotalCars,
+	MyCarsButton,
+} from './styles';
 import { CarDTO } from '../../dtos/CarDTO';
+import { useTheme } from 'styled-components';
 
 export function Home() {
+	const theme = useTheme();
+
 	const [cars, setCars] = useState<CarDTO[]>([] as CarDTO[]);
 	const [isLoading, setIsLoading] = useState(true);
 
@@ -18,6 +29,10 @@ export function Home() {
 
 	function handleCarDetails(car: CarDTO) {
 		navigation.navigate('CarDetails', { car });
+	}
+
+	function handleOpenMyCars() {
+		navigation.navigate('MyCars');
 	}
 
 	useEffect(() => {
@@ -59,6 +74,9 @@ export function Home() {
 					)}
 				/>
 			)}
+			<MyCarsButton onPress={handleOpenMyCars}>
+				<Ionicons name='ios-car-sport' size={32} color={theme.colors.shape} />
+			</MyCarsButton>
 		</Container>
 	);
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -2,6 +2,7 @@ import { FlatList, FlatListProps } from 'react-native';
 import { CarDTO } from '../../dtos/CarDTO';
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
 	flex: 1;
@@ -36,3 +37,15 @@ export const CarList = styled(
 	},
 	showsVerticalScrollIndicator: false,
 })``;
+
+export const MyCarsButton = styled(RectButton)`
+	width: 60px;
+	height: 60px;
+	background-color: ${({ theme }) => theme.colors.main};
+	justify-content: center;
+	align-items: center;
+	border-radius: 30px;
+	position: absolute;
+	bottom: 13px;
+	right: 22px;
+`;
