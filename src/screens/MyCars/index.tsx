ecb17c43bb0b6bfe93b92d18f485345721dd6527
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-04 00:43:35 -0300

    get user scheduling

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { CarDTO } from '../../dtos/CarDTO';
+import { api } from '../../services/api';
 
-import {
-	Container
-} from './styles';
+import { Container } from './styles';
 
 export function MyCars() {
-	return (
-		<Container>
+	const [cars, setCars] = useState<CarDTO[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-		</Container>
-	);
+	useEffect(() => {
+		async function fetchCars() {
+			try {
+				const response = await api.get(`/schedules_byuser?user_id=1`);
+				setCars(response.data);
+			} catch (error) {
+				console.log('erro ao listar agendamentos', error);
+			} finally {
+				setIsLoading(false)
+			}
+		}
+		
+		fetchCars();
+	}, []);
+
+	return <Container></Container>;
 }
