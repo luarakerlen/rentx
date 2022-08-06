150c5b834a519b841c9395676b3538621e84b95a
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-06 19:47:59 -0300

    forms accessibility

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {
+	StatusBar,
+	KeyboardAvoidingView,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from 'react-native';
 import { useTheme } from 'styled-components';
 import { Button, Input, PasswordInput } from '../../components';
 
@@ -9,53 +14,54 @@ export function SignIn() {
 	const theme = useTheme();
 
 	return (
-		<Container>
-			<StatusBar
-				barStyle='dark-content'
-				translucent
-				backgroundColor='transparent'
-			/>
-			<Header>
-				<Title>
-					Estamos{'\n'}
-					quase lá.
-				</Title>
-				<SubTitle>
-					Faça seu login para começar{'\n'}
-					uma experiência incrível.
-				</SubTitle>
-			</Header>
+		<KeyboardAvoidingView behavior='position' enabled>
+			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+				<Container>
+					<StatusBar
+						barStyle='dark-content'
+						translucent
+						backgroundColor='transparent'
+					/>
+					<Header>
+						<Title>
+							Estamos{'\n'}
+							quase lá.
+						</Title>
+						<SubTitle>
+							Faça seu login para começar{'\n'}
+							uma experiência incrível.
+						</SubTitle>
+					</Header>
 
-			<Form>
-				<Input
-					iconName='mail'
-					placeholder='E-mail'
-					keyboardType='email-address'
-					autoCorrect={false}
-					autoCapitalize='none'
-				/>
-				<PasswordInput
-					iconName='lock'
-					placeholder='Senha'
-				/>
-			</Form>
+					<Form>
+						<Input
+							iconName='mail'
+							placeholder='E-mail'
+							keyboardType='email-address'
+							autoCorrect={false}
+							autoCapitalize='none'
+						/>
+						<PasswordInput iconName='lock' placeholder='Senha' />
+					</Form>
 
-			<Footer>
-				<Button
-					title='Login'
-					onPress={() => {}}
-					enabled={false}
-					isLoading={false}
-				/>
-				<Button
-					title='Criar conta gratuita'
-					color={theme.colors.background_secondary}
-					onPress={() => {}}
-					enabled={true}
-					isLoading={false}
-					light
-				/>
-			</Footer>
-		</Container>
+					<Footer>
+						<Button
+							title='Login'
+							onPress={() => {}}
+							enabled={false}
+							isLoading={false}
+						/>
+						<Button
+							title='Criar conta gratuita'
+							color={theme.colors.background_secondary}
+							onPress={() => {}}
+							enabled={true}
+							isLoading={false}
+							light
+						/>
+					</Footer>
+				</Container>
+			</TouchableWithoutFeedback>
+		</KeyboardAvoidingView>
 	);
 }
