9745a756721abb69c690588596d5c45ff62b7861
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-08 01:36:53 -0300

    custom bottom tab

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,20 +1,59 @@
 import React from 'react';
-
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Home, MyCars } from '../screens';
+
+import HomeSvg from '../assets/home.svg';
+import CarSvg from '../assets/car.svg';
+import PeopleSvg from '../assets/people.svg';
+
+import { MyCars } from '../screens';
 import { AppStackRoutes } from './app.stack.routes';
+import { useTheme } from 'styled-components';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppTabRoutes() {
+	const theme = useTheme();
+
 	return (
 		<Navigator
-		screenOptions={{
-			headerShown: false,
-		}}>
-			<Screen name='Home' component={AppStackRoutes} />
-			<Screen name='MyCars' component={MyCars} />
-			{/* <Screen name='Profile' component={Profile} /> */}
+			screenOptions={{
+				headerShown: false,
+				tabBarActiveTintColor: theme.colors.main,
+				tabBarInactiveTintColor: theme.colors.text_detail,
+				tabBarShowLabel: false,
+				tabBarStyle: {
+					paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+					height: 78,
+					backgroundColor: theme.colors.background_primary,
+				},
+			}}
+		>
+			<Screen
+				name='Home'
+				component={AppStackRoutes}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<HomeSvg width={24} height={24} fill={color} />
+					),
+				}}
+			/>
+			<Screen
+				name='MyCars'
+				component={MyCars}
+				options={{
+					tabBarIcon: ({ color }) => (
+						<CarSvg width={24} height={24} fill={color} />
+					),
+				}}
+			/>
+			{/* <Screen
+				name='Profile'
+				component={Profile}
+				options={{
+					tabBarIcon: ({ color }) => <PeopleSvg width={24} height={24} fill={color} />,
+				}}
+			/> */}
 		</Navigator>
 	);
 }
