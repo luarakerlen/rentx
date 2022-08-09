92e7d99e9d8b17225fb97dd1ca1daf4e12a2d49a
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-09 19:35:41 -0300

    create header of profile screen

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+import { Feather } from '@expo/vector-icons';
+
+import { BackButton } from '../../components';
 
 import {
-	Container
+	Container,
+	Header,
+	HeaderTop,
+	HeaderTitle,
+	LogoutButton,
+	PhotoContainer,
+	Photo,
+	PhotoButton,
 } from './styles';
 
 export function Profile() {
+	const theme = useTheme();
+
+	function handleLogout() {}
+
 	return (
 		<Container>
+			<StatusBar
+				barStyle='light-content'
+				translucent
+				backgroundColor='transparent'
+			/>
+			<Header>
+				<HeaderTop>
+					<BackButton color={theme.colors.shape} />
+					<HeaderTitle>Editar Perfil</HeaderTitle>
+					<LogoutButton onPress={handleLogout}>
+						<Feather name='power' size={24} color={theme.colors.shape} />
+					</LogoutButton>
+				</HeaderTop>
 
+				<PhotoContainer>
+					<Photo source={{uri: 'https://avatars.githubusercontent.com/u/26902816?v=4'}}/>
+					<PhotoButton onPress={() => {}}>
+						<Feather name='camera' size={24} color={theme.colors.shape}/>
+					</PhotoButton>
+				</PhotoContainer>
+			</Header>
 		</Container>
 	);
 }
