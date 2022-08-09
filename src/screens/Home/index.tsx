e0bd9dc13ec86b285412a6467df2676379a443de
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-09 15:19:18 -0300

    memory leak

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -65,18 +65,27 @@ export function Home() {
 	}
 
 	useEffect(() => {
+		let isMounted = true;
+
 		async function fetchCars() {
 			try {
 				const response = await api.get('/cars');
-				setCars(response.data);
+				if (isMounted) {
+					setCars(response.data);
+				}
 			} catch (error) {
 				console.log('erro fetchCars: ', error);
 			} finally {
-				setIsLoading(false);
+				if (isMounted) {
+					setIsLoading(false);
+				}
 			}
 		}
 
 		fetchCars();
+		return () => {
+			isMounted = false;
+		};
 	}, []);
 
 	return (
