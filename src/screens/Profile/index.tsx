6f5413ca4b84fe7e1f087a54c04bb6cc610cc4ea
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-10 17:57:17 -0300

    signout confirmation

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -91,6 +91,23 @@ export function Profile() {
 		}
 	}
 
+	async function handleSignOut() {
+		Alert.alert(
+			'Tem certeza?',
+			'Se você sair, precisará de internet para conectar-se novamente.',
+			[
+				{
+					text: 'Cancelar',
+					onPress: () => {},
+				},
+				{
+					text: 'Sair',
+					onPress: () => signOut(),
+				},
+			]
+		);
+	}
+
 	return (
 		<KeyboardAvoidingView behavior='position' enabled>
 			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -104,7 +121,7 @@ export function Profile() {
 						<HeaderTop>
 							<BackButton color={theme.colors.shape} />
 							<HeaderTitle>Editar Perfil</HeaderTitle>
-							<LogoutButton onPress={signOut}>
+							<LogoutButton onPress={handleSignOut}>
 								<Feather name='power' size={24} color={theme.colors.shape} />
 							</LogoutButton>
 						</HeaderTop>
